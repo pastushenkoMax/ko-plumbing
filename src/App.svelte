eb44316fb6141505e8006620b87e46d761ccde0a
pastushenkoMax <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>
2025-03-23 09:42:32 -0600

    first-components add

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,49 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script lang="ts">
+  // import NavBar from "./lib/components/nav/NavBar.svelte"
+  // import SecondaryNavBar from "./lib/components/nav/SecondaryNavBar.svelte"
+  // import Hero from "./lib/components/hero/Hero.svelte"
+  // import About from "./lib/components/about/About.svelte"
+  // import Services from "./lib/components/services/Services.svelte"
+  // import Contacts from "./lib/components/contacts/Contacts.svelte"
+
+  import Form from "./lib/Form.svelte";
+
+  import { onMount, onDestroy } from "svelte"
+
+  let showSecondaryNav = false
+
+  function handleScroll() {
+    showSecondaryNav = window.scrollY > 20
+  }
+
+  onMount(() => {
+    window.addEventListener("scroll", handleScroll)
+  })
+
+  onDestroy(() => {
+    window.removeEventListener("scroll", handleScroll)
+  })
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <h1>new start</h1>
+  <Form />
+  <!-- {#if showSecondaryNav}
+    <SecondaryNavBar />
+  {/if}
+  <div class="top">
+    <NavBar/>
+    <Hero id="home"/>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <About id="about"/>
+  <Services2 id="services"/>
+  <Contacts id="contacts"/> -->
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .top{
+    background: linear-gradient(180deg, #DAEAF5 0%, rgba(218, 234, 245, 0.7) 19.44%, rgba(255, 255, 255, 0) 100%);
   }
 </style>
+

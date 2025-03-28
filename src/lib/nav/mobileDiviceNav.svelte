1b136123f8fa71486c05373cf290872060b43565
pastushenkoMax <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>
2025-03-27 19:43:46 -0600

    bug fix

--- src/lib/nav/mobileDiviceNav.svelte
+++ src/lib/nav/mobileDiviceNav.svelte
@@ -112,97 +112,96 @@
 <style>
 
 
-#menuToggle
-{
-    display: block;
-    position: fixed;
-    top: var(--gap-xbig);
-    right: var(--gap-xbig);
-    z-index: 1;
-    -webkit-user-select: none;
-    user-select: none;
-}
-
-
-#menuToggle input{
-    display: block;
-    width: 26px;
-    height: 24px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    cursor: pointer;
-    opacity: 0;
-    z-index: 2; 
-    -webkit-touch-callout: none;
+#menuToggle{
+        display: block;
+        position: fixed;
+        top: var(--gap-xbig);
+        right: var(--gap-xbig);
+        z-index: 1;
+        -webkit-user-select: none;
+        user-select: none;
     }
 
-#menuToggle span{
-    display: block;
-    width: 28px;
-    height: 4px;
-    margin-bottom: 4px;
-    position: relative;
-    background: var(--accent-clr);
-    border-radius: 3px;
-    z-index: 1;
-    transform-origin: 2px 0px;
-    transition: transform 0.3s cubic-bezier(0.77,0.2,0.05,1.0),
-                background 0.3s cubic-bezier(0.77,0.2,0.05,1.0),
-                opacity 0.45s ease;
-}
 
-#menuToggle span:first-child{
-    transform-origin: 0% 0%;
-}
+    #menuToggle input{
+        display: block;
+        width: 26px;
+        height: 24px;
+        position: absolute;
+        top: 0;
+        left: 0;
+        cursor: pointer;
+        opacity: 0;
+        z-index: 2; 
+        -webkit-touch-callout: none;
+        }
 
-#menuToggle span:nth-last-child(2){
-    transform-origin: 0% 100%;
-}
-
-#menuToggle input:checked ~ span{
-    opacity: 1;
-    transform: rotate(45deg) translate(-2px, -1px);
-    background: var(--accent-clr);
-}
-
-#menuToggle input:checked ~ span:nth-last-child(3){
-    opacity: 0;
-    transform: rotate(0deg) scale(0.2, 0.2);
-}
-
-#menuToggle input:checked ~ span:nth-last-child(2){
-    transform: rotate(-45deg) translate(0, -1px);
-}
-
-#menu{
-    position: absolute;
-    padding: 0 var(--gap-xbig);
-    top: calc(0% - var(--gap-xbig));
-    left: calc(100% + var(--gap-xbig));
-    width: 100vw;
-    background: #ffffff;
-    box-shadow: 20px 20px 40px #414141a9,
-            1px 1px 1px #dadada;
-    -webkit-font-smoothing: antialiased;
-    transform-origin: 0% 0%;
-    transform: translate(100%, 0);
-    transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0);
-}
-
-#menu li{
-    padding: 10px 0;
-    font-size: 22px;
-}
-
-
-#menuToggle input:checked ~ ul{
-    transform: none;
-    transform: translate(-100%, 0);
-}
-
-:global(li.active > button){
-        color: var(--secondery-clr);
+    #menuToggle span{
+        display: block;
+        width: 28px;
+        height: 4px;
+        margin-bottom: 4px;
+        position: relative;
+        background: var(--accent-clr);
+        border-radius: 3px;
+        z-index: 1;
+        transform-origin: 2px 0px;
+        transition: transform 0.3s cubic-bezier(0.77,0.2,0.05,1.0),
+                    background 0.3s cubic-bezier(0.77,0.2,0.05,1.0),
+                    opacity 0.45s ease;
     }
+
+    #menuToggle span:first-child{
+        transform-origin: 0% 0%;
+    }
+
+    #menuToggle span:nth-last-child(2){
+        transform-origin: 0% 100%;
+    }
+
+    #menuToggle input:checked ~ span{
+        opacity: 1;
+        transform: rotate(45deg) translate(-2px, -1px);
+        background: var(--accent-clr);
+    }
+
+    #menuToggle input:checked ~ span:nth-last-child(3){
+        opacity: 0;
+        transform: rotate(0deg) scale(0.2, 0.2);
+    }
+
+    #menuToggle input:checked ~ span:nth-last-child(2){
+        transform: rotate(-45deg) translate(0, -1px);
+    }
+
+    #menu{
+        position: absolute;
+        padding: 0 var(--gap-xbig);
+        top: calc(0% - var(--gap-xbig));
+        left: calc(100% + var(--gap-xbig));
+        width: 100vw;
+        background: #ffffff;
+        box-shadow: 20px 20px 40px #414141a9,
+                1px 1px 1px #dadada;
+        -webkit-font-smoothing: antialiased;
+        transform-origin: 0% 0%;
+        transform: translate(100%, 0);
+        transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0);
+    }
+
+    #menu li{
+        padding: 10px 0;
+        font-size: 22px;
+    }
+
+
+    #menuToggle input:checked ~ ul{
+        transform: none;
+        transform: translate(-100%, 0);
+    }
+
+    #menuToggle :global(li.active > button){
+            color: var(--secondery-clr);
+        }
 
 </style>
